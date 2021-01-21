66fc686fb070addbe59cb0501e51bd415085613c
rachwongrw <rwrachelwong@gmail.com>
2021-01-21 11:30:07 -0800

    add item functionality included

--- ToDude/ToDude/ItemListTableViewController.swift
+++ ToDude/ToDude/ItemListTableViewController.swift
@@ -9,10 +9,41 @@ import UIKit
 import CoreData
 
 class ItemListTableViewController: UITableViewController {
-
+  let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
   var items = [Item]()
   
-    override func viewDidLoad() {
+ 
+  @IBAction func addBtnTapped(_ sender: UIBarButtonItem) {
+    let alertController = UIAlertController(
+      title: "Add item",
+      message: "",
+      preferredStyle: .alert
+    )
+    
+    var tempTextField = UITextField()
+    
+    let alertAction = UIAlertAction(title: "Done", style: .default) { (_) in
+      let newItem = Item(context: self.context)
+      if let text = tempTextField.text {
+        newItem.title = text
+        newItem.completed = false
+        
+        self.items.append(newItem)
+        self.saveiTems()
+      }
+    }
+    
+    alertController.addTextField { (textField) in
+      textField.placeholder = "Todo Title"
+      tempTextField = textField
+    }
+    
+    alertController.addAction(alertAction)
+    
+    present(alertController, animated: true, completion: nil)
+  }
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
     }
 
@@ -20,18 +51,26 @@ class ItemListTableViewController: UITableViewController {
 
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-      return 5 // items.count
+      return items.count
     }
 
   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
-        let index = indexPath.row
+        
         // Configure the cell...
-        cell.textLabel?.text = "Item #"
-        cell.accessoryType = index % 2 == 0 ? .checkmark : .none
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.completed ? .checkmark : .none
         return cell
     }
-
+  
+  func saveiTems() {
+    do {
+      try context.save()
+    } catch {
+      print("Error saving items")
+    }
+    tableView.reloadData()
+  }
 }
